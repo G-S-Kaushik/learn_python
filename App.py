7450f02a39ed440ba8eb739a4926ad5b0790b161
Kaushik <g.skaushik41@gmail.com>
2023-11-05 15:40:07 +0530

    My 1st commit

---
+++ App.py
@@ -0,0 +1,18 @@
+
+def sum(a,b):
+    s=a+b
+    return s
+
+def sub(a,b):
+    min=a-b
+    return min
+
+def multiplication(a,b):
+    multi=a*b
+    return multi
+
+def average(a,b):
+    avg=(a+b)/2
+    return avg
+
+
---
+++ Python.py
@@ -0,0 +1,22 @@
+from App import *
+a=int(input('Enter number: '))
+b=int(input('Enter number2: '))
+sum1=sum(a,b)
+print(f'Sum of {a},{b} is {sum1}')
+
+multi=multiplication(a,b)
+print(f'Multiplication of {a},{b} is {multi}')
+
+avg=average(a,b)
+print(f'Average of {a},{b} is {avg}')
+
+subtraction=sub(a,b)
+print(f'Sub of {a},{b} is {subtraction}')
+
+
+
+
+
+
+
+
